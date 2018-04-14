3f03baf7c34a8afc50b7c3063f83ada9916e2b10
Shubham Jain <sj.iitr@gmail.com>
2018-04-15 00:59:43 +0530

    clean setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # setup the project
 setup(
     name="syncit",
-    version="1.0.1",
+    version="1.0.3",
     url="https://github.com/shhaumb/syncit",
     author="Shubham Jain",
     author_email="sj.iitr@gmail.com",
@@ -13,4 +13,6 @@ setup(
     keywords="asyncio synchronous ast syncit",
     license="MIT",
     packages=find_packages(include=['syncit']),
+    py_modules=['syncit'],
+    python_requires='>=3.5'
 )
