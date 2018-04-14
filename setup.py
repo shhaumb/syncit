5c5c90e56d60028047643c30577934a22b6c1ab2
Shubham Jain <sj.iitr@gmail.com>
2018-04-15 00:41:12 +0530

    clean setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 # setup the project
 setup(
     name="syncit",
-    version="1.0.0",
+    version="1.0.1",
+    url="https://github.com/shhaumb/syncit",
     author="Shubham Jain",
+    author_email="sj.iitr@gmail.com",
     description=(
         "Extract synchronous functions from async coroutine functions "
         "using AST manipulation"),
+    keywords="asyncio synchronous ast syncit",
     license="MIT",
     packages=find_packages(include=['syncit']),
 )
